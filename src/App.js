35909f1636e57f131bb53ca557d0e2ca448efd85
atlas <a.sproson@outlook.com>
2022-09-24 15:16:36 +0100

    Correctly positions carousel buttons within the DOM

--- src/App.js
+++ src/App.js
@@ -10,59 +10,72 @@ const testUrl =
   "https://botw-compendium.herokuapp.com/api/v2/category/monsters";
 // const sheikahSlate = require("./assets/SheikahSlateUpscale.png")
 
-const data = [
-  {
-    category: "monsters",
-    common_locations: ["Hyrule Field", "East Necluda"],
-    description:
-      "This heavyweight species of monster can be found all over Hyrule. They're physically very strong, their legs along strong enough to resist the force of a bomb blast. They're much more dangerous than the Bokoblins. In fact, Moblins have been known to pick up Bokoblins and throw them as makeshift projectile weapons.",
-    drops: ["moblin horn", "moblin fang"],
-    id: 108,
-    image: "https://botw-compendium.herokuapp.com/api/v2/entry/moblin/image",
-    name: "moblin",
-  },
-  {
-    category: "monsters",
-    common_locations: ["Hyrule Field", "Hebra Mountains"],
-    description:
-      "These fearsome monsters have lived in Hyrule since ancient times. Their ability to breathe fire makes White-Maned Lynels among the toughest of the species; each one of their attacks is an invitation to the grave. There are so few eyewitness accounts of this breed because a White-Maned Lynel is not one to let even simple passersby escape with their lives.",
-    drops: ["lynel horn", "lynel hoof", "lynel guts"],
-    id: 123,
-    image:
-      "https://botw-compendium.herokuapp.com/api/v2/entry/white-maned_lynel/image",
-    name: "white-maned lynel",
-  },
-  {
-    category: "monsters",
-    common_locations: null,
-    description:
-      "The lowest-ranked members of the Yiga Clan. They've been dispatched all across Hyrule with a single task: seek out Link and end his life. They're a crafty bunch, sometimes disguising themselves as simple travelers or villagers to get the jump on you. Be wary of suspicious people you encounter. They're very agile and carry a bow and one-handed sword.",
-    drops: [
-      "green rupee",
-      "blue rupee",
-      "red rupee",
-      "purple rupee",
-      "mighty bananas",
-    ],
-    id: 134,
-    image:
-      "https://botw-compendium.herokuapp.com/api/v2/entry/yiga_footsoldier/image",
-    name: "yiga footsoldier",
-  },
-];
+// const data = [
+//   {
+//     category: "monsters",
+//     common_locations: ["Hyrule Field", "East Necluda"],
+//     description:
+//       "This heavyweight species of monster can be found all over Hyrule. They're physically very strong, their legs along strong enough to resist the force of a bomb blast. They're much more dangerous than the Bokoblins. In fact, Moblins have been known to pick up Bokoblins and throw them as makeshift projectile weapons.",
+//     drops: ["moblin horn", "moblin fang"],
+//     id: 108,
+//     image: "https://botw-compendium.herokuapp.com/api/v2/entry/moblin/image",
+//     name: "moblin",
+//   },
+//   {
+//     category: "monsters",
+//     common_locations: ["Hyrule Field", "Hebra Mountains"],
+//     description:
+//       "These fearsome monsters have lived in Hyrule since ancient times. Their ability to breathe fire makes White-Maned Lynels among the toughest of the species; each one of their attacks is an invitation to the grave. There are so few eyewitness accounts of this breed because a White-Maned Lynel is not one to let even simple passersby escape with their lives.",
+//     drops: ["lynel horn", "lynel hoof", "lynel guts"],
+//     id: 123,
+//     image:
+//       "https://botw-compendium.herokuapp.com/api/v2/entry/white-maned_lynel/image",
+//     name: "white-maned lynel",
+//   },
+//   {
+//     category: "monsters",
+//     common_locations: null,
+//     description:
+//       "The lowest-ranked members of the Yiga Clan. They've been dispatched all across Hyrule with a single task: seek out Link and end his life. They're a crafty bunch, sometimes disguising themselves as simple travelers or villagers to get the jump on you. Be wary of suspicious people you encounter. They're very agile and carry a bow and one-handed sword.",
+//     drops: [
+//       "green rupee",
+//       "blue rupee",
+//       "red rupee",
+//       "purple rupee",
+//       "mighty bananas",
+//     ],
+//     id: 134,
+//     image:
+//       "https://botw-compendium.herokuapp.com/api/v2/entry/yiga_footsoldier/image",
+//     name: "yiga footsoldier",
+//   },
+//   {
+//     category: "monsters",
+//     common_locations: [
+//       "Divine Beast Vah Ruta"
+//     ],
+//     description: "This phantom of Ganon attacked the Divine Beast Vah Ruta and was responsible for the demise of the Champion Mipha. It attacks with a spear and ice magic.",
+//     drops: [
+      
+//     ],
+//     id: 161,
+//     image: "https://botw-compendium.herokuapp.com/api/v2/entry/waterblight_ganon/image",
+//     name: "waterblight ganon"
+//   },
+// ];
 
 let counter = 0;
 
 function App() {
-  // const [data, setData] = useState()
+  const [data, setData] = useState()
   const [idx, setIdx] = useState(0);
 
-  // useEffect(() => {
-  //   axios.get(testUrl).then((response) => {
-  //     console.log('calling...')
-  //     setData(response.data.data)
-  //   })
-  // }, [])
+  useEffect(() => {
+    axios.get(testUrl).then((response) => {
+      console.log('calling...')
+      setData(response.data.data)
+    })
+  }, [])
 
   const handleClickForward = () => {
     if (counter < data.length - 1) {
@@ -157,8 +170,11 @@ function App() {
                 </div>
               </div>
 
+            </div>
+
+
               {/* Carousel buttons */}
-              <div className="flex justify-between -mt-[194px] ml-[140px] w-[795px]">
+              <div className="absolute mt-[237px] ml-[140px] w-[800px] flex justify-between">
                 <div className="">
                   <button onClick={handleClickBackward}>
                     <MdOutlinePlayArrow className="h-[60px] w-[60px] text-cyan-500 rotate-180 transition hover:scale-125 ease-in-out" />
@@ -171,7 +187,9 @@ function App() {
                   </button>
                 </div>
               </div>
-            </div>
+
+
+              
           </div>
         </div>
       </div>
