1ccf379c16c333765216fda56f338ff4e66e1008
atlas <a.sproson@outlook.com>
2022-09-24 17:50:22 +0100

    Completes header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className="mt-[100px]">
-            <h1>Hello</h1>
-        </div>
-    );
+  return (
+    <div className="absolute mt-[90px] w-full left-0 right-0 ml-auto mr-auto text-center">
+      <h1 className="font-zelda text-[100px] ">Hyrule Compendium</h1>
+      <div className="-mt-[10px] w-[500px] absolute left-0 right-0 ml-auto mr-auto">
+        <p className="mb-4">Welcome!</p>
+        <p className="mb-4">
+          This demo project renders data from a free online web API that stores
+          data from the video game{" "}
+          <span className="italic">Legend of Zelda: Breath of the Wild</span>
+        </p>
+        <p>
+          Clicking on the left or right arrows will cycle through the data and
+          present various facts about the monsters crawling across Hyrule!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
