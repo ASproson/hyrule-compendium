6a30d4352531b511ca56ba75aad8fb3c568e2d0e
atlas <a.sproson@outlook.com>
2022-09-24 14:11:39 +0100

    Functioning carousel

--- src/App.js
+++ src/App.js
@@ -21,13 +21,31 @@ const data = [
   },
   {
     category: "monsters",
-    common_locations: ["Hyrule Field", "East Necluda"],
+    common_locations: ["Hyrule Field", "Hebra Mountains"],
     description:
-      "This heavyweight species of monster can be found all over Hyrule. They're physically very strong, their legs along strong enough to resist the force of a bomb blast. They're much more dangerous than the Bokoblins. In fact, Moblins have been known to pick up Bokoblins and throw them as makeshift projectile weapons.",
-    drops: ["moblin horn", "moblin fang"],
-    id: 108,
-    image: "https://botw-compendium.herokuapp.com/api/v2/entry/moblin/image",
-    name: "moblin 2",
+      "These fearsome monsters have lived in Hyrule since ancient times. Their ability to breathe fire makes White-Maned Lynels among the toughest of the species; each one of their attacks is an invitation to the grave. There are so few eyewitness accounts of this breed because a White-Maned Lynel is not one to let even simple passersby escape with their lives.",
+    drops: ["lynel horn", "lynel hoof", "lynel guts"],
+    id: 123,
+    image:
+      "https://botw-compendium.herokuapp.com/api/v2/entry/white-maned_lynel/image",
+    name: "white-maned lynel",
+  },
+  {
+    category: "monsters",
+    common_locations: null,
+    description:
+      "The lowest-ranked members of the Yiga Clan. They've been dispatched all across Hyrule with a single task: seek out Link and end his life. They're a crafty bunch, sometimes disguising themselves as simple travelers or villagers to get the jump on you. Be wary of suspicious people you encounter. They're very agile and carry a bow and one-handed sword.",
+    drops: [
+      "green rupee",
+      "blue rupee",
+      "red rupee",
+      "purple rupee",
+      "mighty bananas",
+    ],
+    id: 134,
+    image:
+      "https://botw-compendium.herokuapp.com/api/v2/entry/yiga_footsoldier/image",
+    name: "yiga footsoldier",
   },
 ];
 
@@ -48,19 +66,19 @@ function App() {
   // if(!data) <p>Loading...</p>
 
   const handleClickForward = () => {
-    if(counter < data.length -1) {
-      counter ++
+    if (counter < data.length - 1) {
+      counter++;
     } else {
-      counter = 0
+      counter = 0;
     }
     setIdx(counter);
   };
 
   const handleClickBackward = () => {
-    if(counter > 0) {
-      counter --
+    if (counter > 0) {
+      counter--;
     } else {
-      counter = data.length - 1
+      counter = data.length - 1;
     }
     setIdx(counter);
   };
@@ -109,15 +127,30 @@ function App() {
                   </div>
                   <div className="flex mt-2 text-md">
                     <h2 className="font-bold">Common locations:</h2>
-                    {data[idx].common_locations.map((locale) => {
-                      return <p className="ml-2 capitalize">{locale}</p>;
-                    })}
+                    <div>
+                      {data[idx].common_locations === null ? (
+                        <p className="ml-2">Unknown</p>
+                      ) : (
+                        <div>
+                          {data[idx].common_locations.map((locale) => {
+                            return (
+                              <span key={locale} className="ml-2 capitalize">
+                                {locale}
+                              </span>
+                            );
+                          })}
+                        </div>
+                      )}
+                    </div>
                   </div>
                   <h2 className="font-bold mt-2">Drops:</h2>
                   <div className="flex mt-2 text-md capitalize space-x-6">
-                    {data[idx].drops.map((drops) => {
+                    {data[idx].drops.slice(0, 5).map((drops) => {
                       return (
-                        <p className="text-white text-sm bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full p-3 cursor-pointer">
+                        <p
+                          key={drops}
+                          className="text-white text-sm bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full p-3 cursor-pointer -ml-[180px]"
+                        >
                           {drops}
                         </p>
                       );
@@ -130,7 +163,6 @@ function App() {
         </div>
         <button onClick={handleClickForward}>Increment</button>
         <button onClick={handleClickBackward}>Decrement</button>
-
       </div>
       <div
         className="w-full h-[35px] absolute bottom-0"
