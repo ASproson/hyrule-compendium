082482d55da66dbd071f65659b7549b83490701c
atlas <a.sproson@outlook.com>
2022-09-25 09:01:29 +0100

    Removes top livery banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,10 +4,10 @@ import triforce from "../assets/triforce.png";
 const Banner = () => {
   return (
     <div className="w-screen h-screen absolute -z-50">
-      <div
+      {/* <div
         className="w-full h-[90px] absolute  top-0 rotate-180"
         style={{ backgroundImage: `url(${triforce})` }}
-      ></div>
+      ></div> */}
       <div
         className="w-full h-[90px] absolute bottom-0"
         style={{ backgroundImage: `url(${triforce})` }}
