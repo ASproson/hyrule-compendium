4339e66390b9db9b4d2969dda42ab3b08d8d7e63
atlas <a.sproson@outlook.com>
2022-09-25 09:38:36 +0100

    Sets mobile styling

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,7 +5,7 @@ import { MdOutlinePlayArrow } from "react-icons/md";
 const Carousel = ({handleClickBackward, handleClickForward}) => {
   return (
     <div>
-      <div className="absolute w-[800px] flex justify-between">
+      <div className="absolute md:w-[800px] flex justify-between">
         <div className="">
           <button onClick={handleClickBackward}>
             <MdOutlinePlayArrow className="h-[60px] w-[60px] text-cyan-500 rotate-180 transition hover:scale-125 ease-in-out animate-pulse" />
