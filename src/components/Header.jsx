6a8a87a5e75f2a3650bece4f75b5221634d6aba7
atlas <a.sproson@outlook.com>
2022-09-25 09:00:16 +0100

    Changes header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className="absolute mt-[120px] w-full left-0 right-0 ml-auto mr-auto text-center">
       <h1 className="font-zelda text-[50px] sm:text-[70px] lg:text-[100px]">Hyrule Compendium</h1>
-      <div className="-mt-[10px] w-[350px] md:w-[500px] absolute left-0 right-0 ml-auto mr-auto">
+      <div className="-mt-[10px] w-[400px] md:w-[500px] absolute left-0 right-0 ml-auto mr-auto">
         <p className="mb-4">Welcome...</p>
         <p className="mb-4">
-          Clicking on the left or right arrows will cycle through the data and
+          Clicking on the left or right arrows in the Sheikah slate will cycle through the data and
           present various facts about the monsters crawling across Hyrule as seen in the <span className="italic">Legend of Zelda: Breath of the Wild!</span>
         </p>
       </div>
